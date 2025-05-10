6e9cc6d1af12486d9f48818f144aa94a6c286109
gjoel114 <drgakwayajoel@gmail.com>
2025-05-11 03:35:41 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/grading-web/', // IMPORTANT!
-  plugins: [react()]
+  plugins: [react()],
 });
